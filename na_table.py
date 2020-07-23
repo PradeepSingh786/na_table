d643de0bc8e31840662042ce75dfbbc1a584bf49
adityadebroy02 <63453524+adityadebroy02@users.noreply.github.com>
2020-07-23 14:23:09 +0530

    Code File
    Code for NA table which will give output for NA and and it's percentage for respective columns

---
+++ na_table.py
@@ -0,0 +1,7 @@
+def na_table(data):
+    import pandas as pd 
+    na_table=pd.DataFrame()
+    na_table['Total NA']=data.isna().sum()
+    na_table['Percent NA']=round(data.isna().sum()/data.isna().count()*100,2)
+    
+    return na_table
