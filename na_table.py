935ee897c52acb26596108bdf5561d577be0f9b9
PradeepSingh786 <68184972+PradeepSingh786@users.noreply.github.com>
2020-09-07 18:20:52 +0530

    Update na_table.py
    Comments

--- na_table.py
+++ na_table.py
@@ -1,7 +1,8 @@
 def na_table(data):
-    import pandas as pd 
+    import pandas as pd
     na_table=pd.DataFrame()
     na_table['Total NA']=data.isna().sum()
     na_table['Percent NA']=round(data.isna().sum()/data.isna().count()*100,2)
-    
+
     return na_table
+# yeah that's great programme you made.
